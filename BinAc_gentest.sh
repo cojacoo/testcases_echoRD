73b2acd0f17376ef9296a837cbddeea7fed8519d
cojack <conrad.jackisch@kit.edu>
2017-05-10 11:35:28 +0200

    cd to path in script

--- BinAc_gentest.sh
+++ BinAc_gentest.sh
@@ -20,35 +20,37 @@ echo "- - - - - - - - - - -\n"
 echo "This script shall run all generic echoRD test cases on a BinAC node."
 echo "- - - - - - - - - - -\n"
 
+cd /home/ka/ka_iwg/ka_oj4748/echoRD/testcases_echoRD
+
 module load numlib/numpy
 module load lib/matplotlib
 module load lib/pandas
 module load numlib/scipy
 
-python ~/echoRD/testcases_echoRD/gen_test1111.py &
-python ~/echoRD/testcases_echoRD/gen_test1212.py &
-python ~/echoRD/testcases_echoRD/gen_test2121.py &
-python ~/echoRD/testcases_echoRD/gen_test2222.py &
-python ~/echoRD/testcases_echoRD/gen_test3211.py &
-python ~/echoRD/testcases_echoRD/gen_test1112.py &
-python ~/echoRD/testcases_echoRD/gen_test1221.py &
-python ~/echoRD/testcases_echoRD/gen_test2122.py &
-python ~/echoRD/testcases_echoRD/gen_test3111.py &
-python ~/echoRD/testcases_echoRD/gen_test3212.py &
-python ~/echoRD/testcases_echoRD/gen_test1121.py &
-python ~/echoRD/testcases_echoRD/gen_test1222.py &
-python ~/echoRD/testcases_echoRD/gen_test2211.py &
-python ~/echoRD/testcases_echoRD/gen_test3112.py &
-python ~/echoRD/testcases_echoRD/gen_test3221.py &
-python ~/echoRD/testcases_echoRD/gen_test1122.py &
-python ~/echoRD/testcases_echoRD/gen_test2111.py &
-python ~/echoRD/testcases_echoRD/gen_test2212.py &
-python ~/echoRD/testcases_echoRD/gen_test3121.py &
-python ~/echoRD/testcases_echoRD/gen_test3222.py &
-python ~/echoRD/testcases_echoRD/gen_test1211.py &
-python ~/echoRD/testcases_echoRD/gen_test2112.py &
-python ~/echoRD/testcases_echoRD/gen_test2221.py &
-python ~/echoRD/testcases_echoRD/gen_test3122.py &
-python ~/echoRD/testcases_echoRD/gen_test1123.py &
-python ~/echoRD/testcases_echoRD/gen_test2123.py &
-python ~/echoRD/testcases_echoRD/gen_test3123.py
+python gen_test1111.py &
+python gen_test1212.py &
+python gen_test2121.py &
+python gen_test2222.py &
+python gen_test3211.py &
+python gen_test1112.py &
+python gen_test1221.py &
+python gen_test2122.py &
+python gen_test3111.py &
+python gen_test3212.py &
+python gen_test1121.py &
+python gen_test1222.py &
+python gen_test2211.py &
+python gen_test3112.py &
+python gen_test3221.py &
+python gen_test1122.py &
+python gen_test2111.py &
+python gen_test2212.py &
+python gen_test3121.py &
+python gen_test3222.py &
+python gen_test1211.py &
+python gen_test2112.py &
+python gen_test2221.py &
+python gen_test3122.py &
+python gen_test1123.py &
+python gen_test2123.py &
+python gen_test3123.py
