cbcd59a4c254019508d1581c5f7aa1ed02f3ff17
cojack <conrad.jackisch@kit.edu>
2017-05-10 11:00:33 +0200

    added path of files and single file test

---
+++ BinAc_gentest_X.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=24:00:00
+#PBS -l mem=12gb
+#PBS -S /bin/bash
+#PBS -N echoRD_gentests_X
+#PBS -j oe
+#PBS -o LOG
+#PBS -n
+
+#cd /beegfs/work/ka_oj4748
+
+echo "User:"
+whoami
+echo "Job running on node:"
+uname -a
+echo "started on "
+date
+echo "- - - - - - - - - - -\n"
+echo "This script shall test-run one generic echoRD test cases on a BinAC node."
+echo "- - - - - - - - - - -\n"
+
+module load numlib/numpy
+module load lib/matplotlib
+module load lib/pandas
+module load numlib/scipy
+
+python ~/echoRD/testcases_echoRD/gen_test1111.py
