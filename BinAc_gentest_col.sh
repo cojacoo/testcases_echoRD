aaf768e19ce59c8cb8546b791a3d297b072a5298
cojack <conrad.jackisch@kit.edu>
2017-05-10 16:22:19 +0200

    added column tests

--- BinAc_gentest_col.sh
+++ BinAc_gentest_col.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=28
+#PBS -l walltime=72:00:00
 #PBS -l mem=12gb
 #PBS -S /bin/bash
-#PBS -N echoRD_gentests_X
+#PBS -N echoRD_cols
 #PBS -j oe
-#PBS -o LOG
+#PBS -o LOG_col
 #PBS -n
 
 #cd /beegfs/work/ka_oj4748
@@ -17,7 +17,7 @@ uname -a
 echo "started on "
 date
 echo "- - - - - - - - - - -\n"
-echo "This script shall test-run one column echoRD test cases on a BinAC node."
+echo "This script shall test-run all column echoRD test cases on a BinAC node."
 echo "- - - - - - - - - - -\n"
 
 cd /home/ka/ka_iwg/ka_oj4748/echoRD/testcases_echoRD
@@ -27,4 +27,27 @@ module load lib/matplotlib
 module load lib/pandas
 module load numlib/scipy
 
-python gen_test_col1.py
+python gen_test_col1.py &
+python gen_test_col2.py &
+python gen_test_col3.py &
+python gen_test_col4.py &
+python gen_test_col5.py &
+python gen_test_col6.py &
+python gen_test_coR1.py &
+python gen_test_coR2.py &
+python gen_test_coR3.py &
+python gen_test_coR4.py &
+python gen_test_coR5.py &
+python gen_test_coR6.py &
+python gen_test_cXl1.py &
+python gen_test_cXl2.py &
+python gen_test_cXl3.py &
+python gen_test_cXl4.py &
+python gen_test_cXl5.py &
+python gen_test_cXl6.py &
+python gen_test_cXR1.py &
+python gen_test_cXR2.py &
+python gen_test_cXR3.py &
+python gen_test_cXR4.py &
+python gen_test_cXR5.py &
+python gen_test_cXR6.py
