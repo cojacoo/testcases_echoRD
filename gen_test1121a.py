471a9e7b7b7833fcde7d34e4c88428c2399d70b3
cojack <conrad.jackisch@kit.edu>
2017-06-02 11:30:44 +0200

    runfiles corrected

--- gen_test1121a.py
+++ gen_test1121a.py
@@ -10,4 +10,4 @@ import sys
 sys.path.append(pathdir)
 
 import run_echoRD as rE
-rE.echoRD_job(mcinif=mcinif,mcpick=mcpick,runname=runname,wdir=wdir,pathdir=pathdir,update_mf=update_mf,update_prec=update_prec,legacy_pick=legacy_pick)
+rE.echoRD_job(mcinif=mcinif,mcpick=mcpick,runname=runname,wdir=wdir,pathdir=pathdir,update_prec=update_prec,legacy_pick=legacy_pick)
