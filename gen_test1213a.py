13da80c60711aae28a5858db0690d1b57e8d9684
cojack <conrad.jackisch@kit.edu>
2017-06-02 11:20:14 +0200

    new batch job files (adapted to new echoRD_job)

---
+++ gen_test1213a.py
@@ -0,0 +1,13 @@
+mcinif='mcini_gen1a'
+mcpick='gen_test1a.pickle'
+runname='gen_test1213'
+pathdir='/beegfs/work/ka_oj4748/echoRD/echoRD'
+wdir='/beegfs/work/ka_oj4748/echoRD/echoRD_weierbach'
+update_prec=0.06
+legacy_pick=True
+
+import sys
+sys.path.append(pathdir)
+
+import run_echoRD as rE
+rE.echoRD_job(mcinif=mcinif,mcpick=mcpick,runname=runname,wdir=wdir,pathdir=pathdir,update_mf=update_mf,update_prec=update_prec,legacy_pick=legacy_pick)
