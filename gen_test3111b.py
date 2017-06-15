4e0181d2f4f2a2413014caac4b8d72e6f10aed5c
cojack <conrad.jackisch@kit.edu>
2017-06-15 16:34:47 +0200

    testcases with identical macropores added

--- gen_test3111b.py
+++ gen_test3111b.py
@@ -4,7 +4,7 @@ mcpick='gen_test3b.pickle'
 pathdir='/beegfs/work/ka_oj4748/echoRD'
 wdir='/beegfs/work/ka_oj4748/gen_tests'
 update_prec=0.04
-update_mf='False'
+update_mf=False
 update_part=500
 
 import sys
