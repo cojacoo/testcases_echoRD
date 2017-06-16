7599e775bb13a86243dbda08cd4879feebd13112
cojack <conrad.jackisch@kit.edu>
2017-06-16 12:28:58 +0200

    realworld tests corrected

--- colpach01.py
+++ colpach01.py
@@ -4,13 +4,14 @@ mcpick='colpach.pick'
 pathdir='/beegfs/work/ka_oj4748/echoRD'
 wdir='/beegfs/work/ka_oj4748/gen_tests'
 import sys
+from pathlib import Path
 import numpy as np
 sys.path.append(pathdir)
 
 macscale=0.1
 
 runname=runnamex+str(int(np.round(macscale*10))).zfill(2)
-if macscale!=1. and ''.join([wdir,'/results/Z',runname,'_Mstat.pick']).is_file():
+if macscale!=1. and Path(''.join([wdir,'/results/Z',runname,'_Mstat.pick'])).is_file():
 	print('resuming into stored run')
 else:
 	runname=runnamex
